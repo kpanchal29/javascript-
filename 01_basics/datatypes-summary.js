12bc0d21b6ab6c8f7940989caf6ba5f3d1080011
Krishit panchal <krishitpanchal2909@gmail.com>
2024-02-24 01:35:17 +0530

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.03
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+// Reference (Non - Primitive)
+
+// Array, Objects, Functions
+
+//array
+const heros = ["ironman","hulk"]  
+//object
+let myobj = {                    
+    name:"krishit",
+    age:"21",
+}
+// function
+const myFunction = function(){   
+    console.log("hello world");
+}
+
+
+
