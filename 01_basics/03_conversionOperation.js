f8e0e880a6fae8f26ae061e7b95c7125e0f766a9
Krishit panchal <krishitpanchal2909@gmail.com>
2024-02-24 01:10:00 +0530

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,18 @@ let score = "krishit";
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+//*******************Operations*****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " krishit"
+let str3 = str1 + str2
+console.log(str3);
