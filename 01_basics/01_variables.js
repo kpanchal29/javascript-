12024c789014efaa626c4cc45680dda96779e9fb
Krishit panchal <krishitpanchal2909@gmail.com>
2024-02-24 00:30:09 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 128331
+let accountEmail = "12313@gmail.com"
+var accountPassword = "12345"
+accountCity = "idar"
+let accountState;
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "dada@d.com"
+accountPassword = "331331"
+accountCity = "udaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3);// we are using node.js, not browser
+
+console.log(3 + 3);
+
+
+//string
+//boolean
+//number
+//undefined
+//null
+//object
+//symbol
+
+console.log(typeof "krishit");
+console.log(typeof undefined);
+console.log(typeof null);
