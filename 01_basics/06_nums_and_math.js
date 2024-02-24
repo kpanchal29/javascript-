34d09e2fa343fafe146668dc4a6777c97dffe033
Krishit panchal <krishitpanchal2909@gmail.com>
2024-02-24 18:56:47 +0530

    math and nums in js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const balance = 50000
+console.log(balance);
+
+const score = new Number(100)
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hun = 10000000
+console.log(hun.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++Math++++++++++++++++++++++++++++=
+
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.9));
+console.log(Math.floor(Math.random()*6) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1) + min));
+
+
+
+
+
+
+
