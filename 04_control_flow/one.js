7e93601efb728199ac0020b7ad47af93c2ea6024
Krishit panchal <krishitpanchal2909@gmail.com>
2024-03-01 00:26:38 +0530

    Control Flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// if
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+
+}
+
+const score = 200
+
+// if (score > 100) {
+    // const power = "fly"
+    // console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);/
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900){
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user Logged in");
+    
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,9 @@
+const userEmail = ""
+
+if (userEmail) {
+    console.log("Got user email");
+    
+} else {
+    console.log("don't have user email");
+}
+
