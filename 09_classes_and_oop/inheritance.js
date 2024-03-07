c19bcfc1e3f740ce7ae50627302a4b9050a3f37d
Krishit panchal <krishitpanchal2909@gmail.com>
2024-03-07 23:57:56 +0530

    inheritance in js

---
+++ 09_classes_and_oop/inheritance.js
@@ -0,0 +1,31 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai","chai@teacher.com","123")
+
+chai.addCourse()
+
+const masalaChai = new User("kpanchal")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
---
+++ 09_classes_and_oop/myClasses.js
@@ -0,0 +1,47 @@
+// ES6
+
+// class User {
+//     constructor(username, email, password){
+//     this.username = username
+//     this.email = email
+//     this.password = password
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+
+//     changeUsername(){
+//         return`${this.username.toUpperCase()}`
+//     }
+
+// }
+
+// const chai = new User("kp","kp@gmail.com","123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username
+    this.email = email
+    this.password = password
+
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("krishit","kp@gmail.com","123")
+
+console.log(tea.changeUsername());
+console.log(tea.encryptPassword());
+
+
